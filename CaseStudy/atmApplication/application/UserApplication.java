a44c2c3ad7057f2023775b642eb360f4bc02c44e
gary.lim <gary.lim@OPT-HO-26S1B92.optimum.net>
2019-08-06 10:06:37 +0800

    Case Study ( Without DataBase )

---
+++ CaseStudy/atmApplication/application/UserApplication.java
@@ -0,0 +1,15 @@
+package application;
+
+import controller.Controller;
+
+public class UserApplication
+{
+
+	public static void main(String[] args) 
+	{
+		Controller refControl = new Controller();
+		refControl.adminController(refControl); //Passing the object reference to minimize the creation of object
+		// naving conveion tryCatch
+	}
+
+}
